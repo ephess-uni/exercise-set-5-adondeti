c06e6767fb5d7a01170c5309cae90b7b80c09933
adondeti <150321140+adondeti@users.noreply.github.com>
2023-12-13 14:41:08 +0300

    Update ex_5_4.py

--- src/ex_5_4.py
+++ src/ex_5_4.py
@@ -17,5 +17,8 @@ input_file = data_dir / "ex_5_4-data.csv"
 output_file = output_dir / "ex_5_4-processed.csv"
 
 # Process the input data using numpy
+dta = np.loadtxt(input_file)
 
+dta[dta < 0] = 0
 # Save the result to output_file
+np.savetx(output_file, dta, fmt = "%.2fe")
